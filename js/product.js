c1c11d29cad37d23b4ae666819334e4ec762c63a
MohdAzzam <alazzamhmode@gmail.com>
2021-02-28 22:33:50 +0200

    adding js and fix product page

---
+++ js/product.js
@@ -0,0 +1,44 @@
+/* eslint-disable indent */
+'use strict';
+const cart = new Cart([]);
+
+// let products= document.getElementById('products');
+console.log(allCar);
+// console.log(allCar[0].filePath);
+let indeximage = 1;
+
+for (let i = 0; i < 8; i++) {
+    document.getElementById('image-' + indeximage).setAttribute('src', allCar[i].filePath);
+    document.getElementById('img-brand-' + indeximage).textContent = allCar[i].kind;
+    document.getElementById('car-name-' + indeximage).textContent = allCar[i].name;
+    document.getElementById('car-price-' + indeximage).textContent = allCar[i].price;
+    let targetRow = document.getElementById("add-to-cart-btn-" + indeximage);
+    targetRow.setAttribute("data-price", allCar[i].price);
+    targetRow.setAttribute("data-name", allCar[i].name);
+    indeximage++;
+    if (indeximage > 8) {
+        break;
+    }
+}
+
+let userCart = [];
+function addToCart(targetId) {
+    let target = document.getElementById(targetId);
+    let price = target.getAttribute("data-price");
+    let name = target.getAttribute("data-name");
+    let car = {
+        price: price,
+        name: name
+    };
+
+    
+    if (localStorage.getItem("user-cart")) {
+        userCart = JSON.parse(localStorage.getItem("user-cart"));
+    }
+
+    userCart.push(car);
+    console.log(JSON.stringify(userCart));
+    localStorage.setItem("user-cart", JSON.stringify(userCart));
+    console.log('added');
+
+}
